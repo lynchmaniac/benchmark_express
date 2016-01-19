e6320152e802ad494278dd65d610f0e9a55e9a73
lynchmaniac <lynchmaniac@gmail.com>
2016-01-19 21:03:25 +0100

    first commit

---
+++ routes/artistes.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/export', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
+});
+/*
+ * POST an artist.
+ */
+router.post('/', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        var info = { type: "artist" };
+        var artist = { href: "https://api.monsite.com/artistes/" + Math.round(Math.random()*99999) + 1, name: req.body.nom, nb_album: req.body.nb_album };
+        var json = JSON.stringify({ 
+          info: info, 
+          artist: artist
+        });
+        res.setHeader('Content-Type', 'application/json');
+        res.status(201);
+        res.send(json);
+        /*res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );*/
+    });
+})
+/*
+ * GET an artist 
+ */
+router.get('/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.findOne({'_id' : req.params.id}, function (err, doc) {
+      if(err) return done(err);
+      var json = JSON.stringify({ 
+        artist: doc
+      });
+      //res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200);
+      res.send(json);
+    });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: 'artist supprimé' } : { msg:'error: ' + err });
+    });
+});
+
+/*
+ * GET all artist.
+ */
+router.get('/', function(resq, res) {
+
+var json = JSON.stringify({
+ "href": "https://api.monsite.com/recherche/artistes?q=U2", 
+ "info": {
+    "num_results": 3,
+    "limit": 100,
+    "offset": 0,
+    "query": "U2",
+    "type": "artist",
+    "page": 1
+  },
+  "artists": [
+    {
+      "href": "https://api.monsite.com/artistes/15",
+      "name": "U2",
+      "genres" : [ "rock", "indie pop" ],
+      "nb_album" : 13,
+      "popularity": "0.76"
+    },
+    {
+      "href": "https://api.monsite.com/artistes/1922",
+      "name": "U2kushi",
+      "genres" : [ "electro" ],
+      "nb_album" : 1,
+      "popularity": "0.12"
+    },
+    {
+      "href": "https://api.monsite.com/artistes/623",
+      "name": "U2R",
+      "genres" : [ "new wave" ],
+      "nb_album" : 1,
+      "popularity": "0.41"
+    },
+  ]
+});
+    res.setHeader('Content-Type', 'application/json');
+    res.send(json);
+});
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Database
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('mongodb://urgbovlr8lbrykv:CyOpq0zckGezBHw5PNHO@bdd8a1axxuhwyct-mongodb.services.clever-cloud.com:27017/bdd8a1axxuhwyct');
+
+var artistes = require('./routes/artistes');
+
+var app = express();
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+     req.db = db;
+     next();
+});
+
+
+app.use('/artistes', artistes);
+
+
+
+
+
+
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+/// IMPORT DU WWWW
+
+/**
+ * Module dependencies.
+ */
+
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(8080);
+
+module.exports = app;
